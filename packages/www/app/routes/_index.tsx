5fd8331832f6d16d3f14a1800677cf2a6d7bb60f
Bryan Skye <32001892+BryanSky@users.noreply.github.com>
2024-10-15 14:52:44 +0200

    Fix statistics on landing page (#125)
    * Fix statistics on landing page
    
    * Remove unnecessary log statement
    
    * Set default to 0, to have increasing numbers
    
    * Adress review issues and add cache
    
    ---------
    
    Co-authored-by: lindner <benedict.lindner@synthavo.de>

--- packages/www/app/routes/_index.tsx
+++ packages/www/app/routes/_index.tsx
@@ -1,11 +1,12 @@
-import type { LinksFunction, MetaFunction } from "@remix-run/node";
-import { useState, useEffect } from "react";
+import { LinksFunction, MetaFunction } from "@remix-run/node";
 import HeroSection from "../components/HeroSection";
 import StorySection from "../components/StorySection";
 import FeaturesSection from "../components/FeaturesSection";
 import RoadmapSection from "../components/RoadmapSection";
 import ContributionSection from "../components/ContributionSection";
 import Footer from "../components/Footer";
+import { LRUCache } from "lru-cache";
+import { useLoaderData } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -18,13 +19,56 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+const cache = new LRUCache<
+  string,
+  { stars: number; contributors: number; blocks: number; workflows: number }
+>({
+  max: 100,
+  ttl: 1000 * 60 * 15, // Cache expiration time: 15 minutes
+});
+
+export const loader = async () => {
+  const repoUrl = "https://api.github.com/repos/softflow24/data-river";
+  const cacheKey = "storySectionStats";
+  try {
+    const cachedStats = cache.get(cacheKey);
+    if (cachedStats) {
+      return cachedStats;
+    }
+    const [repoData, contributorsData] = await Promise.all([
+      fetch(repoUrl).then((res) => {
+        if (!res.ok) throw new Error("Failed to fetch repo data");
+        return res.json();
+      }),
+      fetch(repoUrl + "/contributors").then((res) => {
+        if (!res.ok) throw new Error("Failed to fetch contributors data");
+        return res.json();
+      }),
+    ]);
+
+    const result = {
+      stars: Number(repoData.stargazers_count) ?? 0,
+      contributors: Number(contributorsData.length) ?? 0,
+      blocks: 5,
+      workflows: 0.4, // this is rounded to 0 in number-ticker, but if you put directly 0, nothing will be displayed
+    };
+    cache.set(cacheKey, result);
+    return result;
+  } catch (err: any) {
+    console.error(
+      err.message || "Something went wrong while fetching GitHub stats",
+    );
+    return {
+      stars: 0,
+      contributors: 0,
+      blocks: 5,
+      workflows: 0.4, // this is rounded to 0 in number-ticker, but if you put directly 0, nothing will be displayed
+    };
+  }
+};
+
 export default function Index() {
-  const [stats, setStats] = useState({
-    stars: 123,
-    contributors: 123,
-    nodes: 123,
-    workflows: 123,
-  });
+  const stats = useLoaderData<typeof loader>();
 
   return (
     <>
