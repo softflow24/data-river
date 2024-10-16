044e109b20fab3de567cf6ee76a5de32de352e38
kyle-ignis <kyle@ignistech.io>
2024-10-16 13:15:41 -0600

    adding request block class for HTTP requests

--- packages/blocks/src/plugins/PluginRegistration.ts
+++ packages/blocks/src/plugins/PluginRegistration.ts
@@ -2,4 +2,8 @@ import { registerBlockType } from "../blockFactory";
 
 import { DatabaseBlock } from "./DatabaseBlock";
 
+import { RequestBlock } from "./RequestBlock";
+
 registerBlockType("database", DatabaseBlock);
+
+registerBlockType("request", RequestBlock);
