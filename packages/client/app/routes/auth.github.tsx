f5e0a604912774453c04147cceaa303f8e46980f
girl-loves-coding <girllovescoding@gmail.com>
2024-11-06 23:11:16 +0100

    fix: adjust github auth redirect url

--- packages/client/app/routes/auth.github.tsx
+++ packages/client/app/routes/auth.github.tsx
@@ -6,15 +6,11 @@ export async function action({ request }: ActionFunctionArgs) {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "github",
     options: {
-      redirectTo: `${new URL(request.url).origin}/sign-up`,
+      redirectTo: `${new URL(request.url).origin}/auth/callback`,
     },
   });
 
   if (error) throw error;
 
   return redirect(data.url);
-}
-
-export async function loader() {
-  return redirect("/sign-up");
 }
